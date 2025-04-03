f8035eef8280be9d33827bfb8806f4b46eed4db3
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-03 07:53:26 +0530

    get products array

---
+++ src/hooks/useApi.ts
@@ -0,0 +1,18 @@
+export interface Product{
+    id: number,
+    title: string,
+    price: number,
+    description: string,
+    category: string,
+    image: string,
+    rating: {
+        rate: number,
+        count: number
+    }
+}
+
+export default async function getProducts(): Promise<Product[]>{
+    const res:Response = await fetch("https://fakestoreapi.com/products");
+    const products:Product[] = await res.json();
+    return products
+}
