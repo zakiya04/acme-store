f74f4e78117b2c285be8a8c42a30bada94f2e97d
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-14 18:43:42 +0530

    add basic structure

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -5,7 +5,7 @@ import Product from './product';
 const Products: React.FC = () => {
   const {products} = useProducts()
   return (
-    <div>
+    <div className='grid gap-4 grid-cols-3 w-[75%] m-auto p-4'>
       {products.map(product => (
         <div key={product.id}>
           <Product product={product}/>
