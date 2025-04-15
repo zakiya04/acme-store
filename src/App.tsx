fe230a72487e734f4156cb5c55d482e0aa52731f
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-15 20:10:48 +0530

    add hooks for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Products from "./components/products";
+import Details from "./components/productDetails";
+import { Routes, Route, Link } from "react-router-dom";
 
 const App: React.FC = () => {
   
   return (
-    <h1 className="bg-white">hello</h1>
+    <>
+    <Link to='/products' className="bg-white p-4 hover:bg-gray-500">Go to Products</Link>
+    <Routes>
+    <Route path="/" element={<App />} />
+      <Route path='/products' element={<Products/>}/>
+      <Route path='/products/:id' element={<Details/>}/>
+    </Routes>
+    </> 
   )
 };
 
