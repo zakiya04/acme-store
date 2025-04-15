7c94f4c43a12800713889dce50a0a706bbb6b718
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-15 20:08:50 +0530

    add route for cart

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -8,13 +8,13 @@ type Props = {
 const Product: React.FC <Props> = ({product}) => {
   return (
     <div className='border rounded-2xl border-gray-900 p-4 bg-gray-800' >
-      <Link to='/products/'>
+      <Link to={`/products/${product.id}`} state={{product}}>
         <div>
             <img src={product.image} className='h-75 w-75'></img>
         </div>
         <div className='px-2 py-4 flex items-center justify-center gap-4'>
             <p className='truncate'>{product.title}</p>
-            <button className='text-white '>${product.price}</button>
+            <button className='text-white cursor-pointer'>${product.price}</button>
         </div>
       </Link>
     </div>
