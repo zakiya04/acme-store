2e43594c1d5554c4aacf77fefdbd9447f43494b9
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-17 19:42:44 +0530

    import remix icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Products from "./components/products";
 import Details from "./pages/productDetails";
+import Header from "./components/header";
 import { Routes, Route, Link } from "react-router-dom";
 
 const App: React.FC = () => {
   
   return (
     <>
+    <Header/>
     <Link to='/products' className="bg-white p-4 hover:bg-gray-500">Go to Products</Link>
+    
     <Routes>
     <Route path="/" element={<App />} />
       <Route path='/products' element={<Products/>}/>
---
+++ src/components/header.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.png";
+import { RiShoppingCart2Fill } from "react-icons/ri";
+
+const Header: React.FC = () => {
+  return (
+    <div className="flex items-center justify-between pb-4  ">
+      <div className="w-1/3 flex items-center justify-around p-4">
+        <Link to="/" className="flex items-center justify-center">
+          <div className="w-[40px] h-[40px] mr-2 border-[0.5px] border-gray-600 rounded-2xl">
+            <img src={logo} className="w-full h-full object-cover rounded-2xl"></img>
+          </div>
+          <h1 className="text-white font-semibold text-medium">ACME STORE</h1>
+        </Link>
+        <Link to="/products" className="text-gray-600">All</Link>
+        <Link to="/ptoducts/" className="text-gray-600">Men's</Link>
+        <Link to="" className="text-gray-600">Women's</Link>
+      </div>
+      <div className="w-1/3  p-4 ">
+        <input placeholder='Search for Products...' className="border-[0.5px] border-gray-500 px-3 py-2 w-full h-[40px] rounded-xl"></input>
+      </div>
+      <div className="w-1/3 flex items-center justify-end p-4 ">
+        <div  className="h-[40px] border border-gray-700 w-[40px] rounded-sm flex items-center justify-center">
+          <RiShoppingCart2Fill className="text-gray-500 w-[50%] h-[50%]"/>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
