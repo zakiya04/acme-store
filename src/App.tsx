948d81cedccf9dd1cc3a2119779f4a2f9921ba86
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-18 21:42:09 +0530

    testing app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,23 @@
 import React from "react";
 import Products from "./components/products";
 import Details from "./pages/productDetails";
-import Header from "./components/header";
+import Home from "./pages/home";
+import Cart from "./pages/cart";
 import { Routes, Route, Link } from "react-router-dom";
 
 const App: React.FC = () => {
   
   return (
     <>
-    <Header/>
+    <Home/>
     <Link to='/products' className="bg-white p-4 hover:bg-gray-500">Go to Products</Link>
-    
     <Routes>
-    <Route path="/" element={<App />} />
       <Route path='/products' element={<Products/>}/>
       <Route path='/products/:id' element={<Details/>}/>
+      <Route path='/cart' element={<Cart/>}/>
     </Routes>
     </> 
+
   )
 };
 
