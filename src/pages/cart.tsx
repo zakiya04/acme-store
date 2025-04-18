bebd259eadb87b804729ea58a27e10cf03f89793
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-18 21:41:14 +0530

    new cart page

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Cart:React.FC = () => {
 
 
   return (
-    <div>
-      
+    <div className='p-5 border-l-[0.5px] w-full m-6 h-full bg-amber-400 '>
+      <div>
+        <h1 className=''></h1>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <Link to='/product:id'></Link>
     </div>
   )
 }
