22a82c2e193a1f01cccfca70598c0a838fc5c469
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-21 15:09:03 +0530

    update add and remove function

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -1,10 +1,11 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-type CartItem = {
+export type CartItem = {
   id : string;
   title: string;
   price: number;
   qty: number;
+  image:string;
 }
 type CartState = {
     cart: CartItem[],
@@ -14,7 +15,7 @@ const products= localStorage.getItem('cart');
 
 const initialState:CartState = {
     cart: products ? JSON.parse(products) : [],
-    isOpen : false
+    isOpen : true
 }
 const cartSlice = createSlice({
     name:'cart',
@@ -22,10 +23,10 @@ const cartSlice = createSlice({
     reducers:{
         addToCart: (state,action: PayloadAction<CartItem>) => {
             const product = action.payload
-            let updatedCart
+            let updatedCart: CartItem[]
             const exist = state.cart.find(x => x.id == product.id);
             if(exist){
-               updatedCart = state.cart.map(item => item.id == product.id ? {...item, qty: item.qty + 1} : item)
+               updatedCart = state.cart.map(item => item.id == product.id ? {...item, qty: item.qty + 1, price: item.price * (item.qty + 1)} : item)
             }
             else{
                updatedCart = [...state.cart,{...product, qty:1}]
@@ -35,13 +36,13 @@ const cartSlice = createSlice({
         },
         removeFromCart: (state,action:PayloadAction<CartItem>)=>{
             const product = action.payload
-            let updatedCart
+            let updatedCart: CartItem[]
             const exist2 = state.cart.find (item => item.id == product.id);
             if (exist2 && exist2.qty === 1){
                updatedCart = state.cart.filter(item => item.id !== product.id)
             }
             else{
-               updatedCart = state.cart.map(item => item.id === product.id ? {...item, qty: item.qty - 1} : item)
+               updatedCart = state.cart.map(item => item.id === product.id ? {...item, qty: item.qty - 1, price: product.price * (item.qty - 1)} : item)
             }
             localStorage.setItem('cart', JSON.stringify(updatedCart));
             return {...state, cart: updatedCart}
@@ -49,13 +50,10 @@ const cartSlice = createSlice({
         toggleCart: (state) => {
             state.isOpen = !state.isOpen;
         },
-        closeCart: (state) => {
-            state.isOpen = false;
-        }
     }
 });
 
-export const { addToCart, removeFromCart ,toggleCart, closeCart} = cartSlice.actions;
+export const { addToCart, removeFromCart ,toggleCart} = cartSlice.actions;
 export default cartSlice.reducer;
 
 
