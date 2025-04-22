9078e270d42e65ba2bd8767dc3c65fdeee6737d5
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-22 20:46:02 +0530

    add item positioning

---
+++ src/components/ThreeGrid.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { useProducts } from "../hooks/useApi";
+import ProductCart from "./ProductCart";
+
+const ThreeGrid: React.FC = () => {
+  const { products } = useProducts();
+  const top3 = products.slice(0, 3);
+  if (top3.length === 0) {
+    return <div className="text-white">Loading...</div>; 
+  }
+  return (
+    <div className="p-5  h-[450px] flex items-center justify-between w-full rounded ">
+      <div className="h-full  w-[70%]">
+        <ProductCart product={top3[0]} className="h-full"/>
+      </div>
+      <div className="h-full  flex flex-col w-[29%] gap-2">
+       <ProductCart product={top3[1]} className="h-[50%]"/>
+       <ProductCart product={top3[2]} className="h-[50%]"/>
+      </div>
+    </div>
+  );
+};
+
+export default ThreeGrid
