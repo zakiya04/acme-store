43647a0d19b816dfbfa63f41afcf6c3ef3d72847
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-23 21:45:22 +0530

    app broke

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,23 +1,19 @@
-import React from 'react'
+import React from "react";
 import Header from "../components/header";
 import Cart from "../pages/cart";
-import { useSelector } from 'react-redux';
-import { RootState } from '../redux/store';
+import { useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import ThreeGrid from "../components/ThreeGrid";
 
 const Home: React.FC = () => {
-  const showCart = useSelector((state:RootState)=> state.cart.isOpen)
+  const showCart = useSelector((state: RootState) => state.cart.isOpen);
   return (
-    <div className=' min-w-screen flex'>
-      <div className={`${showCart ? 'w-3/5':'w-full'} h-[100px]`}> 
-        <Header />
-      </div>
-      {showCart &&(
-        <div className="w-2/5 h-[600px] flex items-center justify-center "> 
-        <Cart />
-      </div>
-      )}
-    </div>
-  )
-}
+    <>
+      <Header />
+      <Cart />
+      <ThreeGrid />
+    </>
+  );
+};
 
-export default Home
+export default Home;
