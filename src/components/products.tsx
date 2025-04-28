afb6a3d78086453a8bef43d4245937da02542805
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-28 19:12:13 +0530

    add more width

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import {useProducts} from '../hooks/useApi';
-import Product from './product';
+import React from "react";
+import { useProducts } from "../hooks/useApi";
+import Product from "./product";
+import Links from "../components/Links";
 
 const Products: React.FC = () => {
-  const {products} = useProducts()
+  const { products } = useProducts();
   return (
-    <div className='grid gap-4 grid-cols-3 w-[75%] m-auto p-4'>
-      {products.map(product => (
-        <div key={product.id}>
-          <Product product={product}/>
+    <>
+    <Links/>
+      <div className="grid gap-4 grid-cols-3 w-[85%] m-auto p-4">
+        {products.map((product) => (
+          <div key={product.id}>
+            <Product product={product} />
           </div>
-      ))}
-    </div>
-  )
-}
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Products;
