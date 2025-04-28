21890cf88904e9faf7a27bee62c3b90f37c315f4
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-28 19:11:22 +0530

    add related component

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,19 @@ import Products from "./components/products";
 import Details from "./pages/productDetails";
 import Home from "./pages/home";
 import Cart from "./pages/cart";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Related from './components/Related'
 
 const App: React.FC = () => {
   
   return (
     <>
-    <Home/>
-    <Link to='/products' className="bg-white p-4 hover:bg-gray-500">Go to Products</Link>
     <Routes>
+    <Route path="/" element={<Home/>} /> 
       <Route path='/products' element={<Products/>}/>
       <Route path='/products/:id' element={<Details/>}/>
       <Route path='/cart' element={<Cart/>}/>
+      <Route path='/products/category/:catName' element={<Related/>}/>
     </Routes>
     </> 
 
