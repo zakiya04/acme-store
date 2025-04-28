c69f240ed352ed63828ddb6f5e3852b74aa535af
Zakiya <zakiyamanzoor04@gmail.com>
2025-04-28 19:09:52 +0530

    resolve routing issue

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import Header from "../components/header";
 import Cart from "../pages/cart";
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
 import Carousal from "../components/ImageCarousal";
 import ThreeGrid from "../components/ThreeGrid";
 
 const Home: React.FC = () => {
-  const showCart = useSelector((state: RootState) => state.cart.isOpen);
   return (
     <>
       <Header />
