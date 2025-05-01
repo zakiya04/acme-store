77dac2902d449a2becbaef913aa1f48871470f86
Zakiya <zakiyamanzoor04@gmail.com>
2025-05-01 22:17:07 +0530

    same add to cart problem

--- src/redux/cartSlice.ts
+++ src/redux/cartSlice.ts
@@ -1,11 +1,10 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ProductItem } from '../hooks/useApi';
+import {toast} from 'react-hot-toast'
 
 export type CartItem = {
-  id : string;
-  title: string;
-  price: number;
-  qty: number;
-  image:string;
+    product : ProductItem;
+    qty: number
 }
 type CartState = {
     cart: CartItem[],
@@ -15,7 +14,7 @@ const products= localStorage.getItem('cart');
 
 const initialState:CartState = {
     cart: products ? JSON.parse(products) : [],
-    isOpen : true
+    isOpen : false
 }
 const cartSlice = createSlice({
     name:'cart',
@@ -24,25 +23,27 @@ const cartSlice = createSlice({
         addToCart: (state,action: PayloadAction<CartItem>) => {
             const product = action.payload
             let updatedCart: CartItem[]
-            const exist = state.cart.find(x => x.id == product.id);
+            const exist = state.cart.find(x => x.product.id === product.product.id);
             if(exist){
-               updatedCart = state.cart.map(item => item.id == product.id ? {...item, qty: item.qty + 1, price: item.price * (item.qty + 1)} : item)
+               updatedCart = state.cart.map(item => item.product.id === product.product.id ? {...item, qty: item.qty + 1} : item)
             }
             else{
-               updatedCart = [...state.cart,{...product, qty:1}]
+               updatedCart = [...state.cart,product]
             }
+            toast.success('Added To Cart')
             localStorage.setItem('cart', JSON.stringify(updatedCart));
             return {...state, cart: updatedCart}
+           
         },
         removeFromCart: (state,action:PayloadAction<CartItem>)=>{
             const product = action.payload
             let updatedCart: CartItem[]
-            const exist2 = state.cart.find (item => item.id == product.id);
+            const exist2 = state.cart.find (item => item.product.id ===  product.product.id);
             if (exist2 && exist2.qty === 1){
-               updatedCart = state.cart.filter(item => item.id !== product.id)
+               updatedCart = state.cart.filter(item => item.product.id !== product.product.id)
             }
             else{
-               updatedCart = state.cart.map(item => item.id === product.id ? {...item, qty: item.qty - 1, price: product.price * (item.qty - 1)} : item)
+               updatedCart = state.cart.map(item => item.product.id === product.product.id ? {...item, qty: item.qty - 1} : item)
             }
             localStorage.setItem('cart', JSON.stringify(updatedCart));
             return {...state, cart: updatedCart}
